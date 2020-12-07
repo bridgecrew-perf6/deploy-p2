0a2a5d59826bedd66e5f7daaad11d989571c42bb
KienPham <54146788+kienpham9819@users.noreply.github.com>
2020-12-07 09:36:31 +0700

    Update TaskListRepository.php

--- app/Repositories/TaskList/TaskListRepository.php
+++ app/Repositories/TaskList/TaskListRepository.php
@@ -28,9 +28,9 @@ class TaskListRepository extends BaseRepository implements TaskListRepositoryInt
         if ($taskList) {
             $activities = $taskList->tasks()
                 ->where('is_completed', true)
-                ->orderBy('tasks.updated_at', 'desc')
-                ->select(DB::raw('count(tasks.updated_at) as activities'), DB::raw('date(tasks.updated_at) as date'))
                 ->groupBy('date')
+                ->orderBy('tasks.updated_at', 'desc')
+                ->select(DB::raw('count(tasks.updated_at) as activities, date(tasks.updated_at) as date'))
                 ->get();
 
             return $activities;
